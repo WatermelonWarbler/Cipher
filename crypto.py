85a4669455f7582268e23876cbdaf038a143571e
Noah King <Noah141518@icloud.com>
2017-03-26 13:43:00 -0500

    added customizable shift and a decode feature

--- crypto.py
+++ crypto.py
@@ -1,10 +1,11 @@
 def main():
     word=input("enter word\n")
     out=[]
+    shift=input("enter number\n")
     for c in word:
-        o=chr(ord(c)+1)
+        o=chr(ord(c)+int(shift))
         out.append(o)
     print(''.join(out))
-
+    
 if __name__ == "__main__":
     main()
